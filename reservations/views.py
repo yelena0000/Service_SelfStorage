c4aecc9ccd8f5124d6dae359adb5ebc594fe701c
MrGrose <grachev.ro@gmail.com>
2025-01-22 16:57:05 +0300

    Update files

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Client
 
-# Create your views here.
+def display_statistics(request):
+    stats = Client.get_statistics()
+    return render(request, 'admin/statistics_change_list.html', {'stats': stats})
